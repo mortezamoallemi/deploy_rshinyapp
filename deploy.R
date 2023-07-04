1929bd0c70342c3cee7e7a42c92344fb583a515a
Morteza Moallemi <41530553+mortezamoallemi@users.noreply.github.com>
2023-07-05 08:53:00 +1000

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,7 +1,7 @@
 # Authenticate
-setAccountInfo(name = Sys.getenv("SHINY_ACCOUNT_NAME"),
+rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACCOUNT_NAME"),
                token = Sys.getenv("SHINY_TOKEN"),
                secret = Sys.getenv("SHINY_SECRET"))
 # Deploy
-#deployApp(appFiles = c("ui.R", "server.R", "likes.rds"))
-deployApp(appFiles = c("ui.R", "server.R"))
+#rsconnect::deployApp(appFiles = c("ui.R", "server.R", "likes.rds"))
+rsconnect::deployApp(appFiles = c("ui.R", "server.R"))
