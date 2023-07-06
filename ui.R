f5e2ed681cd9a87dcfdb1975dc5eccaba23f27ae
Morteza Moallemi <41530553+mortezamoallemi@users.noreply.github.com>
2023-07-06 13:42:04 +1000

    Update ui.R

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data Test"),
+    titlePanel("Old Faithful Geyser"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
