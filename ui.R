057c46e35b12c9dac6c5b412c8b28ec9f98add87
Morteza Moallemi <41530553+mortezamoallemi@users.noreply.github.com>
2023-07-12 09:45:52 +1000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,33 +1,48 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 library(shinydashboard)
 library(shinycssloaders)
+library("tm")
 library("wordcloud")
 library("RColorBrewer")
 library("ggplot2")
 library("stringr")
 library(plotly)
+library(shiny)
+library(shinyWidgets) # useShinydashboard()
 
 
-dashboardPage(
-  dashboardHeader(title = "News Sentiment Analysis"),
-  dashboardSidebar(),
-  dashboardBody(
+fluidPage(
+  tags$h2("Classic shiny"),
+  
+  # use this in non shinydashboard app
+  # setBackgroundColor(color = "ghostwhite"),
+  useShinydashboard(),
     # Boxes need to be put in a row (or column)
     fluidRow(
-      box(width = 8,
-          withSpinner(plotlyOutput("different_sentiments", height = 250))),
       box(width = 4,
-          withSpinner(plotOutput("bar_plot", height = 250))),
-      
+          title = "Most Repeated Words",
+          withSpinner(plotOutput("word_cloud")),
+          withSpinner(plotlyOutput("repeated_words", height = 250)),
+          sliderInput("n_words",
+                      "Number of words:",
+                      min = 5,
+                      max = 50,
+                      value = 30)),
       box(width = 4,
-        title = "Most Repeated Words",
-        withSpinner(plotOutput("word_cloud")),
-        sliderInput("n_words",
-                    "Number of words:",
-                    min = 5,
-                    max = 50,
-                    value = 30)
-      )
+          title = "Overall Sentiment",
+          withSpinner(plotOutput("overall_sentiment"))),
+      box(width = 4,
+          withSpinner(plotlyOutput("different_sentiments", height = 250))
+          )
     )
   )
-)
+# )
